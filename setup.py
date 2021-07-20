c55d44be22f0ee74acba5fe172e4cc45e5cf202e
Flamey <morellialex08@gmail.com>
2021-07-20 15:37:13 +0200

    v0.1.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="sussy",
+    version="v0.1.0",
+    author="Flamey",
+    description="when the python developer is sus",
+    packages=["sussy"]
+)
