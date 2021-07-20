157eec96f23926c750d701858d7585d9b8ad45fd
Flamey <morellialex08@gmail.com>
2021-07-20 15:59:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-import setuptools
+from distutils.core import setup
 
-setuptools.setup(
-    name="sussy",
-    version="v0.1.0",
-    author="Flamey",
-    description="when the python developer is sus",
-    packages=["sussy"]
+setup(
+    name = "sussy",
+    packages = ["sussy"],
+    version = "0.1.0",
+    description = "when the python developer is sus",
+    author = "Flamey",
+    url = "https://github.com/ActuallyFlamey/sussy/",
+    download_url = "https://github.com/ActuallyFlamey/sussy/archive/refs/tags/0.1.0.tar.gz"
 )
